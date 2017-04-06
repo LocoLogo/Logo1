5035db59f5576257c2aaf87afeef70dd38d402f9
Courtney Anderson <anderson.2801@osu.edu>
2017-04-06 14:39:53 -0400

    Adds the profile component

--- wwwroot/app/components/Profile/profile.component.ts
+++ wwwroot/app/components/Profile/profile.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    
     selector: 'profile',
     templateUrl: './app/components/profile/profile.component.html'
-    
 })
 
 export class ProfileComponent {
