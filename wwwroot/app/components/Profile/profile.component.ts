da0aefba0685a998748d443f3ecdcab6f648a997
Courtney Anderson <anderson.2801@osu.edu>
2017-04-13 14:02:36 -0400

    Stores all variables from the forms in a class. Profile and Upload components updated.

--- wwwroot/app/components/Profile/profile.component.ts
+++ wwwroot/app/components/Profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormControl }            from '@angular/forms';
 
 @Component({
     selector: 'profile',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
 })
 
 export class ProfileComponent {
-    
+    title = "Profile";
+    //profileForm = null;
+    profileForm;
+    profileError = false;
+    constructor(
+        private fb: FormBuilder,
+        private router: Router) {
+        
+        this.profileForm = fb.group({
+            firstname: ["", Validators.required],
+            lastname: ["", Validators.required],
+            phone: ["", Validators.required],
+            interests: ["", Validators.required],
+            role: ["", Validators.required],
+            about: ["", Validators.required]
+        });
+    }
+    performProfile(e) {
+        var firstname = this.profileForm.value.firstname;
+        var lastname = this.profileForm.value.lastname;
+        var phone = this.profileForm.value.phone;
+        var interests = this.profileForm.value.interests;
+        var role = this.profileForm.value.role;
+        var about = this.profileForm.value.about;
+        alert("First Name: " + firstname);
+    } 
 }
--- wwwroot/app/components/Upload/upload.component.ts
+++ wwwroot/app/components/Upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormControl }            from '@angular/forms';
 
 @Component({
     
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
 })
 
 export class UploadComponent {
-    
+    title = "Upload";
+    //profileForm = null;
+    uploadForm;
+    uploadError = false;
+    constructor(
+        private fb: FormBuilder,
+        private router: Router) {
+        
+        this.uploadForm = fb.group({
+            projectname: ["", Validators.required],
+            projectdepartment: ["", Validators.required],
+            description: ["", Validators.required]
+        });
+    }
+    performUpload(e) {
+        var projectname = this.uploadForm.value.projectname;
+        var projectdepartment = this.uploadForm.value.projectdepartment;
+        var description = this.uploadForm.value.description;
+        alert("Project Name is: " + projectname);
+    }    
 }
