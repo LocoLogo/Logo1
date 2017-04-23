53779043cb5afca5cfe4bc185f51cabace6fcc64
Andrew Benson <abenson@bensonmedia.com>
2017-04-23 15:35:59 -0400

    Added a unit testing class implemented using Xunit

---
+++ Tests/DBConnectionTests.cs
@@ -0,0 +1,46 @@
+﻿using Loco.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace Logo.Test
+{
+    public class DBConnectionTests
+    {
+        MongoClient _client;
+        MongoServer _server;
+        MongoDatabase _db;
+
+        public DBConnectionTests()
+        {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _server = _client.GetServer();
+            _db = _server.GetDatabase("LocoDB");
+        }
+
+        [Fact]
+        public void CreateUserTest()
+        {
+            User u = new User();
+            u.UserName = "TestUserName";
+            u.EmailAddress = "TestUserEmail";
+            u.Interests = "TestUserInterests";
+            u.Role = "TestUserEmail";
+            u.About = "TestUserAbout";
+
+            WriteConcernResult result = _db.GetCollection<User>("Users").Save(u);
+            Assert.Equal(result.HasLastErrorMessage, false);
+        }
+
+        [Fact]
+        public void GetLastUserTest()
+        {
+            User u = _db.GetCollection<User>("Users").FindAll().ToList<User>().LastOrDefault();
+            u.ShouldNotBeNull();
+        }
+    }
+}
---
+++ Tests/SimpleTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using Loco.Models;
+using Moq;
+
+namespace Logo.Test
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
