c3e493f12c4bf7abe321f3f3b6cf747b723b61bd
Courtney Anderson <anderson.2801@osu.edu>
2017-04-24 18:48:43 -0400

    Project Architecture Uploaded

--- Application/wwwroot/app/components/Project/project.component.ts
+++ Application/wwwroot/app/components/Project/project.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
+import { Http, Headers, Response } from '@angular/http';
+import { RequestOptions, Request, RequestMethod} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
 @Component({
     
@@ -7,5 +17,4 @@ import { Component } from '@angular/core';
 })
 
 export class ProjectComponent {
-    
-}
+} 
